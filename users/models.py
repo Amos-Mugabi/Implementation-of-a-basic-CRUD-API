eed0123ff6443b208050ebbcf646694e0e3e77a1
Amos Mugabi <amosmugabi01@gmail.com>
2025-02-18 11:37:54 +0300

    Update models.py
    creating customer user class and initializing email variabl3

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomerUser(AbstractUser):
+    email = models.EmailField(unique = True)
+
+
+
 
 # Create your models here.
