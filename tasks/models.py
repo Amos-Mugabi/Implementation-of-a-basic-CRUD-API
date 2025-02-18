fdf3ac14e2229e947ea1d7ff5d21159c02bf880f
Amos Mugabi <amosmugabi01@gmail.com>
2025-02-18 11:36:58 +0300

    Update models.py
    creating a customer user class

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
+
+from users.models import CustomerUser
+
 from django.db import models
+
+
+
+
+
+class Task(models.Model):
+    title = models.CharField(max_length= 255)
+    description = models.TextField()
+    assigned_to = models.Foreignkey(CustomerUser, on_delete = models)
+
+
+
+
+
+
 
 # Create your models here.
