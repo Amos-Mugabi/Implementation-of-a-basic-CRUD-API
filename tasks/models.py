f63a411e0f2055b89c63c9822a29349e342790ed
Amos Mugabi <amosmugabi01@gmail.com>
2025-02-21 15:52:30 +0300

    update tasks\models.py

--- tasks/models.py
+++ tasks/models.py
@@ -10,7 +10,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length= 255)
     description = models.TextField()
-    assigned_to = models.Foreignkey(CustomerUser, on_delete = models)
+    assigned_to = models.ForeignKey(CustomerUser, on_delete = models.CASCADE)
 
 
 
