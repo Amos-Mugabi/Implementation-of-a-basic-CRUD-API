54867c0fd6d5954868f4f972c7b533068cd47cb2
Amos Mugabi <amosmugabi01@gmail.com>
2025-02-21 14:18:19 +0300

    Update models.py
    initiating customer user class

--- users/models.py
+++ users/models.py
@@ -1,12 +1,21 @@
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser , Group, Permission
 from django.db import models
 
 
 class CustomerUser(AbstractUser):
     email = models.EmailField(unique = True)
 
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_groups",  # Change related_name to avoid clash
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions",  # Change related_name to avoid clash
+        blank=True
+    )
 
 
-
-# Create your models here.
