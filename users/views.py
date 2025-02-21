91628e98f7e26ff037c03eda7a9344fc45aeafd4
Amos Mugabi <amosmugabi01@gmail.com>
2025-02-21 15:53:56 +0300

    update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomerUser
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = CustomerUser.objects.all()
+    serializer_class = UserSerializer
+
+
+
